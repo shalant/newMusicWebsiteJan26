ab8c146cc88fbddd42ead8d34b40fb5090c1f5df
shalant <doug.rosenberg@gmail.com>
2026-01-04 14:03:05 -0600

    updated to dotnet10, got things running

--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -4,11 +4,19 @@ using BlazorApp.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using MudBlazor.Services;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<HeroImageService>();
 
 await builder.Build().RunAsync();
+
+var app = builder.Build();
+
+//app.MapStaticAssets();
