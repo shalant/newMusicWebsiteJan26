03bcd0fd03ecec802331c504c322eaf8f6ed2694
shalant <doug.rosenberg@gmail.com>
2026-01-05 11:45:45 -0600

    adding albums and reviews

---
+++ src/BlazorApp/Models/Album.cs
@@ -0,0 +1,19 @@
+namespace BlazorApp.Models;
+
+public class Album
+{
+    public string Title { get; set; } = string.Empty;
+    public string AlbumCoverUrl { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public List<Review> Reviews { get; set; } = new List<Review>();
+    public string Url { get; set; } = string.Empty;
+}
+
+public class Review
+{
+    public string Author { get; set; } = string.Empty;
+    public string Header { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public int Rating { get; set; }
+
+}
--- src/BlazorApp/Models/Project.cs
+++ src/BlazorApp/Models/Project.cs
@@ -1,8 +1,18 @@
 namespace BlazorApp.Models;
 
-public class Project
-{
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public string Url { get; set; } = string.Empty;
-}
+//public class Album
+//{
+//    public string Title { get; set; } = string.Empty;
+//    public string Description { get; set; } = string.Empty;
+//    public List<Review> Reviews { get; set; } = new List<Review>();
+//    public string Url { get; set; } = string.Empty;
+//}
+
+//public class Review
+//{
+//    public string Author { get; set; } = string.Empty;
+//    public string Header { get; set; } = string.Empty;
+//    public string Content { get; set; } = string.Empty;
+//    public int Rating { get; set; }
+
+//}
