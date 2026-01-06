503d6e6cd918e7a68b143b4c5ca2a61f50488e8d
shalant <doug.rosenberg@gmail.com>
2026-01-05 19:42:42 -0600

    starting work on sheet music section

---
+++ src/BlazorApp/Models/SheetMusic.cs
@@ -0,0 +1,9 @@
+namespace BlazorApp.Models
+{
+    public class SheetMusic
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string URL { get; set; } = string.Empty;
+    }
+}
--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -10,6 +10,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
